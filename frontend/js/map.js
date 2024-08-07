48e4e4f1765cf3f4c6b75cb2e4140f094f717d64
adamkocot1208 <adam.kocot1208@gmail.com>
2024-08-07 15:23:43 +0200

    adding map container and style

--- frontend/js/map.js
+++ frontend/js/map.js
@@ -1,11 +1,21 @@
-// Dodane w całości
-document.addEventListener('DOMContentLoaded', (event) => {
-    // Inicjalizacja mapy
+document.addEventListener('DOMContentLoaded', function() {
+    var mapContainer = document.createElement('div');
+    mapContainer.id = 'map';
+    mapContainer.style.width = '100%';
+    mapContainer.style.height = '100%';
+
+    var bannerSection = document.getElementById('banner');
+    var boxAlt = bannerSection.querySelector('.box.alt');
+    boxAlt.innerHTML = ''; // Clear the placeholder text
+    boxAlt.appendChild(mapContainer);
+
     var map = L.map('map').setView([51.505, -0.09], 13);
 
-    // Dodanie warstwy mapy
     L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+        maxZoom: 19,
     }).addTo(map);
 
+    L.marker([51.5, -0.09]).addTo(map)
+        .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+        .openPopup();
 });
